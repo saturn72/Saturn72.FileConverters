6241a9c7078dd02b07ae362f8f78631d18ff1a3c
Saturn Technologies <info@saturn72.com>
2017-12-22 22:53:24 +0200

    Add method to factory

--- src/Saturn72.FileConverters/FileConverterFactory.cs
+++ src/Saturn72.FileConverters/FileConverterFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Saturn72.FileConverters
@@ -15,6 +17,19 @@ namespace Saturn72.FileConverters
         public virtual IFileConverter GetFileConverter(string sourceExtension, string destinationExtension)
         {
             return _fileConverters?.FirstOrDefault(fc => fc.IsSupported(sourceExtension, destinationExtension));
+        }
+
+        public virtual byte[] Convert(string sourceFormat, string destinationFormat, byte[] bytes)
+        {
+            var fileConverter = GetFileConverter(sourceFormat, destinationFormat);
+            if (fileConverter == null)
+                throw new NotSupportedException(
+                    string.Format("The conversion is not supported. Source: {0} Destination: {1}", sourceFormat,
+                        destinationFormat));
+            using (var ms = new MemoryStream(bytes))
+            {
+                return fileConverter.Convert(sourceFormat, destinationFormat, ms);
+            }
         }
     }
 }
