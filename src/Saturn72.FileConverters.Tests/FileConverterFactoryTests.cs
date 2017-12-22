de917841db0939ae1f969da9062422a80b3e4e17
Saturn Technologies <info@saturn72.com>
2017-12-22 22:37:22 +0200

    Add FileConverterFactory.cs + unit tests

---
+++ src/Saturn72.FileConverters.Tests/FileConverterFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Saturn72.FileConverters.Tests
+{
+    public class FileConverterFactoryTests
+    {
+        [Fact]
+        public void FileConverterFactory_GetFileConverter()
+        {
+            const string srcExt = "src";
+            const string destExt = "dest";
+
+            var fcf1 = new FileConverterFactory(null);
+            fcf1.GetFileConverter(srcExt, destExt).ShouldBeNull();
+
+            var fcs = new Mock<IFileConverter>();
+            var fcf2 = new FileConverterFactory(new[]{fcs.Object});
+            fcf2.GetFileConverter(srcExt, destExt).ShouldBeNull();
+
+            fcs.Setup(f => f.SupportedConversions).Returns(new[] {new FileConversionData(srcExt, destExt)});
+            fcf2.GetFileConverter(srcExt, destExt).ShouldBe(fcs.Object);
+
+        }
+    }
+}
---
+++ src/Saturn72.FileConverters/FileConverterFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Saturn72.FileConverters
+{
+    public class FileConverterFactory
+    {
+        private readonly IEnumerable<IFileConverter> _fileConverters;
+
+        public FileConverterFactory(IEnumerable<IFileConverter> fileConverters)
+        {
+            _fileConverters = fileConverters;
+        }
+
+        public virtual IFileConverter GetFileConverter(string sourceExtension, string destinationExtension)
+        {
+            return _fileConverters?.FirstOrDefault(fc => fc.IsSupported(sourceExtension, destinationExtension));
+        }
+    }
+}
